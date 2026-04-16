23b77afa4ac3db47634f3ff0d36076aa7652390f
farzeenilyaszargar <farzeenilyaszargar@gmail.com>
2026-04-16 10:51:16 +0530

    lol

--- app/install/page.tsx
+++ app/install/page.tsx
@@ -30,7 +30,7 @@ export default function InstallPage() {
                 <div className="relative">
                   <Image src="/i1.png" alt="npm" width={100} height={100} className="w-full z-0" />
                   <CopyCommandBar
-                    text="npm -i napster"
+                    text="npm i -g napster-cli"
                     className="absolute top-[50%] left-[50%] z-10 w-4/5 -translate-x-1/2 -translate-y-1/2 border-0"
                   />
                 </div>
@@ -46,7 +46,7 @@ export default function InstallPage() {
                 <div className="relative">
                   <Image src="/i2.png" alt="npm" width={100} height={100} className="w-full z-0" />
                   <CopyCommandBar
-                    text='Run “nap”'
+                    text='Run “napster”'
                     className="absolute top-[50%] left-[50%] z-10 w-4/5 -translate-x-1/2 -translate-y-1/2 border-0"
                   />
                 </div>
@@ -60,7 +60,7 @@ export default function InstallPage() {
                 <div className="relative">
                   <Image src="/i3.png" alt="npm" width={100} height={100} className="w-full z-0" />
                   <CopyCommandBar
-                    text="npm -i nap/cli"
+                    text="npm update napster-cli"
                     className="absolute top-[50%] left-[50%] z-10 w-4/5 -translate-x-1/2 -translate-y-1/2 border-0"
                   />
                 </div>
--- components/lowHero.tsx
+++ components/lowHero.tsx
@@ -7,7 +7,7 @@ export default function LowHero() {
         <div className="flex flex-col items-center justify-center py-30">
             <h2 className="font-pixelify font-bold text-5xl text-[#828282]">try. <span className="text-[#3F3F3F]">now.</span></h2>
             <div className="flex flex-col justify-between mt-10 gap-5 ">
-                <CopyCommandBar text="npm -i napster" />
+                <CopyCommandBar text="npm i -g napster-cli" />
                 <SmoothScrollLink targetId="features" className=" flex justify-center items-center gap-1">Find out how it works
                 <Image src="/right-arroww.png" alt="right arrow" width={20} height={20} className="rounded-lg " />
 
